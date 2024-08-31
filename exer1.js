b9e78f13bdd3181f00f5688c863dc5eccd0430cb
koitialex <koitialex@gmail.com>
2024-08-30 22:02:36 -0300

    feat: exercício 1

---
+++ exer1.js
@@ -0,0 +1,13 @@
+let n = [];
+let mensagem = "";
+
+for (let i = 0; i < 5; i += 1) {
+    n.push (prompt("Digite um número"));
+}
+for (let i = 0; i < n.length; i++) {
+    mensagem += n[i] + " \n"
+    if (n.length -1)   
+}    
+
+alert (mensagem);
+
